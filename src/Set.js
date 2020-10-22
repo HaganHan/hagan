7853f141b4d95f4a25716b9c2309308c3c0e9530
Hagan <303738305@qq.com>
2020-10-22 09:51:25 +0800

    Set

--- src/Set.js
+++ src/Set.js
@@ -29,6 +29,39 @@ class Set {
   values () {
     return Object.values(this._data)
   }
+  union (set) { // 并集
+    const unionSet = new Set()
+    this.values().forEach(item => unionSet.add(item))
+    set.values().forEach(item => unionSet.add(item))
+    return unionSet
+  }
+  intersection (set) { // 交集
+    const intersectionSet = new Set()
+    if (this.size() <= set.size()) {
+      this.values().forEach(item => {
+        if (set.has(item)) intersectionSet.add(item)
+      })
+    } else {
+      set.values().forEach(item => {
+        if (this.has(item)) intersectionSet.add(item)
+      })
+    }
+    return intersectionSet
+  }
+  difference (set) { // 差集
+    const differenceSet = new Set()
+    this.values().forEach(item => {
+      if (!set.has(item)) differenceSet.add(item)
+    })
+    return differenceSet
+  }
+  isSubsetOf (set) { // 子集
+    if (this.size() > set.size()) return false
+    return this.values().every(item => {
+      if (set.has(item)) return true
+      return false
+    })
+  }
 }
 
 export default Set
