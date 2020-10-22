5c207912f54aba3064a044993e46a62b34970729
Hagan <303738305@qq.com>
2020-10-22 13:37:14 +0800

    Set

--- src/SetMath.js
+++ src/SetMath.js
@@ -1,17 +1,20 @@
 /**
  * Set类的扩展
- * 增加了并集、交集、差集方法
+ * 增加了并集、交集、差集、子集方法
  */
 
 class SetMath extends Set {
-  union (set) {
+  union (set) { // 并集
     return new SetMath([...this, ...set])
   }
-  intersection (set) {
+  intersection (set) { // 交集
     return new SetMath([...this].filter(item => set.has(item)))
   }
-  difference (set) {
+  difference (set) { // 差集
     return new SetMath([...this].filter(item => !set.has(item)))
+  }
+  isSubsetOf (set) { // 子集
+    return [...this].every(item => set.has(item))
   }
 }
 
