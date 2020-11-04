ccd8fc4e3a5938f667d2924d1f7778edc49c9fdf
Hagan <303738305@qq.com>
2020-11-04 09:41:50 +0800

    MinHeap

--- src/MinHeap.js
+++ src/MinHeap.js
@@ -1,17 +1,12 @@
 /**
- * 二叉堆
+ * 二叉堆(最小堆)
  * 它是一颗完全二叉树，表示树的每一层都有左右节点，除了最后一层。并且最后一层的叶节点尽可能都是左侧子节点，这叫结构特性
  * 二叉堆不是最小堆就是最大堆，最小堆允许你快速导出树的最小值，最大堆允许你快速导出树的最大值，所有节点都大于等于最大堆或小于等于最小堆，这叫堆特性
  */
 
-
 function defaultCompare (a, b) {
   if (a === b) return 0
   return a < b ? -1 : 1
-}
-
-function swap (array, a, b) {
-  [array[a], array[b]] = [array[b], array[a]]
 }
 
 class MinHeap {
@@ -51,16 +46,42 @@ class MinHeap {
       parentNodeIndex = this._getParentNodeIndex(index)
     }
   }
-  // 移除并返回最小值
+  // 移除最小值并返回
   extract () {
     if (this.isEmpty()) return
-    if (this.size === 1) return this._heap.shift()
+    if (this.size <= 2) return this._heap.shift()
     const removedValue = this._heap.shift()
     this.siftDown(0) // TODO
     return removedValue
   }
   // 下移操作: TODO
-  siftDown () {
+  siftDown (index) {
+    if (index >= this.size) return
+    const leftChildNodeIndex = this._getLeftChildNodeIndex(index)
+    const rightChildNodeIndex = this._getRightChildNodeIndex(index)
+
+    let targetNodeIndex = index
+    let targetNodeValue = this._heap[targetNodeIndex]
+
+    if (leftChildNodeIndex < this.size) {
+      const leftChildNodeValue = this._heap[leftChildNodeIndex]
+      if (this.compareFn(targetNodeValue, leftChildNodeValue) === 1) {
+        targetNodeIndex = leftChildNodeIndex
+        targetNodeValue = leftChildNodeValue
+      }
+    }
+    if (rightChildNodeIndex < this.size) {
+      const rightChildNodeValue = this._heap[rightChildNodeIndex]
+      if (this.compareFn(targetNodeValue, rightChildNodeValue) === 1) {
+        targetNodeIndex = rightChildNodeIndex
+      }
+    }
+
+    if (index !== targetNodeIndex) {
+      [this._heap[index], this._heap[targetNodeIndex]] = [this._heap[targetNodeIndex], this._heap[index]]
+    }
+    this.siftDown(leftChildNodeIndex)
+    this.siftDown(rightChildNodeIndex)
 
   }
   get size () {
